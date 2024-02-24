6d3dc694cfed948e8aa0916af717bedd48e23317
devkz <sbs@chua-Air.local>
2024-02-24 09:21:17 +0800

    function return type

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import "fmt"
 
 func main() {
 
-	var card string = "Ace of Spades"
-	fmt.Println(card)
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades" // initialization
+	card = "Five of Diamonds"
 
+	card2 := newCard()
+
+	fmt.Println(card)
+	fmt.Println(card2)
+}
+
+func newCard() string {
+	return "Five of Diamond"
 }
