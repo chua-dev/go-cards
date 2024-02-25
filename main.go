76a7427d9e98533c28d7e42d913ac5ec2b65e9d7
devkz <sbs@chua-Air.local>
2024-02-25 10:34:50 +0800

    slice of string

--- main.go
+++ main.go
@@ -5,13 +5,21 @@ import "fmt"
 func main() {
 
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades" // initialization
+	card := "Ace of Spades" // string var init
 	card = "Five of Diamonds"
 
 	card2 := newCard()
 
+	cards := []string{newCard(), "Ace of Diamonds"} // slice init
+	cards = append(cards, "Six of Spades")
+
+	for i, v := range cards {
+		fmt.Println(i, v)
+	}
+
 	fmt.Println(card)
 	fmt.Println(card2)
+	fmt.Println(cards)
 }
 
 func newCard() string {
